23774ced7df5895f1458a56c3b344aff688d8114
Cordial <alicia.cordial@laplateforme.io>
2021-01-04 16:47:40 +0100

    classes

--- classes/user.php
+++ classes/user.php
@@ -1,3 +1,47 @@
-class user{
-    
+<?php
+
+class User{
+    private $id;
+    private $login;
+    private $password;
+    private $password2;
+
+//S'ENREGISTRER
+
+
+//SE CONNECTER
+
+
+//UPDATE
+
+
+
+
+//DECONNEXION
+
+
+
+
+//GETID
+
+
+
+//GETLOGIN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+?>
