6865163ed01ff68e1f273430770728ac8f8909bb
Cordial <alicia.cordial@laplateforme.io>
2021-01-05 11:38:19 +0100

    classes cartes

--- classes/card.php
+++ classes/card.php
@@ -2,6 +2,93 @@
 
 class card {
 
+    private $status;
+    private $card_value;
+    private $id;
+
+
+    //CONSCTRUCTEUR
+
+function __construct($status, $value){
+    $this->status= $status;
+    $this->card_value = $value;
+        
+}
+
+
+    //ID
+
+function id($id){
+        
+    $this->id = $id;
+       
+    return $this->id;
+
+}
+
+
+    //GET STATUS
+
+function get_status(){
+            
+    return $this->status ;
+
+}
+
+
+    //TOURNER LA CARTE
+
+function turn(){
+            
+        if($this->status == "closed"){
+            $this->status = "opened";
+        }
+
+        else{
+            $this->status = "closed"; 
+        }
+            
+    }
+
+
+    //SET STATUS CARTE
+
+function set_status($status){
+
+        $this->status = $status;
+        return $this->status;
+    }
+
+
+
+    //GET VALUE
+
+function get_value(){
+        return $this->card_value;
+    }
+
+
+    // DISPLAY CARTES
+
+function display_cards(){
+
+    if( $this->status == "closed"){
+            
+        echo "<div><a href= \"memory.php?id=$this->id\"><img src =\"src/img/cloud.jpg\" alt=\"carte_retourné\" class=\"covered\"></a></div>";
+    }
+
+    elseif($this->status == "opened"){
+            
+        echo "<div><img src='src/img/IMG$this->card_value.png' alt=\"carte_decouverte\"></div>";
+    }
+    
+    elseif($this->status == "found"){
+        echo "<div><img src='src/img/IMG$this->card_value.png' alt=\"carte_decouverte\"></div>";
+
+    }
+            
+    }
+
 
 
 }
--- classes/score.php
+++ classes/score.php
@@ -9,13 +9,73 @@ class score {
 
     //INSERER SCORE
 
+    function insertScore($id, $level, $time, $moves){
+
+        $this->id = $id;
+        $this->level = $level;
+        $this->time = $time;
+        $this->moves = $moves;
+
+        $req_insert = $this->db->prepare("INSERT INTO score( id_utilisateur, niveau, nb_coup, time) VALUES (?, ?, ?, ?)") ;
+        $req_insert->execute([$this->id , $this->level , $this->moves , $this->time]);
+        
+         return true;
+         
+
+    }
+
+    //RECUPERER RESULTAT
+
+ function fetchScore(){
+
+    $req_fetch = $this->db->prepare("SELECT * from score inner join utilisateurs on score.id_utilisateur =  utilisateurs.id");
+    $req_fetch->execute();
+    
+    $data = $req_fetch->fetchAll();
+
+    return true;
+    }   
+
     //RECUPERER TOUS LES SCORES PAR NIVEAU
+
+
+function scorebyLevel($level){
+        
+    $req_level = $this->db->prepare("SELECT login, niveau, nb_coup, DATE_FORMAT(time, '%i:%s') AS time FROM score inner join utilisateurs on score.id_utilisateur =  utilisateurs.id WHERE niveau = ? ORDER BY score.nb_coup ASC LIMIT 10 ");
+    $req_level->execute([$level]);
+        
+    $data_level = $req_level->fetchAll();
+        
+    return $data_level;
+        
+
+    }
+
+
 
     //RECUPERER TOUS LES SCORES PAR TEMPS
 
+public function scorebytime($level){
+        
+    $req_time = $this->db->prepare("SELECT login, niveau, nb_coup, DATE_FORMAT(time, '%i:%s') AS time FROM score inner join utilisateurs on score.id_utilisateur =  utilisateurs.id WHERE niveau = ? ORDER BY score.time ASC LIMIT 10 ");
+    $req_time->execute([$level]);
+    
+    $data_time =$req_time->fetchAll();
+
+        return $data_time ;
+    }
+
+    
     //RECUPERER LES SCORES PERSOS
 
+    public function scoreUser($id){
 
+        $req_score= $this->db->prepare("SELECT * FROM score WHERE id_utilisateur = ? ORDER BY score.id DESC LIMIT 10");
+        $req_score->execute([$id]);
+        $data_user =  $req_score->fetchAll();
+
+        return $data_user;
+    }
 
 }
 ?>
