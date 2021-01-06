e24282c7d44fa572018316c8dc1c1392adad3991
Cordial <alicia.cordial@laplateforme.io>
2021-01-06 16:12:33 +0100

    board

--- classes/board.php
+++ classes/board.php
@@ -6,38 +6,8 @@ require_once('card.php');
 class board {
 
 
-//function construct
 
 
-
-    //function shuffle
-    function my_str_shuffle($str)
-    {
-        if ($str == '') {
-            return $str;
-        }
-    
-        $n_left = strlen($str);
-    
-        while (--$n_left) {
-            $rnd_idx = mt_rand(0, $n_left);
-            if ($rnd_idx != $n_left) {
-                $tmp = $str[$n_left];
-                $str[$n_left] = $str[$rnd_idx];
-                $str[$rnd_idx] = $tmp;
-            }
-        }
-    
-        return $str;
-    }
-
-
-//function displayBoard
-
-
-
-
-//function turnCard
 
 
 
