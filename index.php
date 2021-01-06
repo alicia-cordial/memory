074f8cc82649e8e3e999f025b35d66ac615c7184
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-01-06 11:48:52 +0100

    index

--- index.php
+++ index.php
@@ -1,92 +1,95 @@
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!--Import Google Icon Font-->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-  
-  <link rel= "stylesheet" href= "index.css">
-  <title>Blue Moon</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Entree - Memory</title>
 </head>
+
 <body>
-
-  <div class="navbar-fixed">
+<div class="navbar-fixed">
     <nav class="nav-wrapper black">
-      <div class="container">
-        <a href="#" class="brand-logo">Blue Moon
-        <i class="material-icons text-white">brightness_4</i>
-        </a>
-        <a href="#" class="sidenav-trigger" data-target="mobile-links">
-          <i class="material-icons">menu</i>
-        </a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="../memory/pages/connexion.php">Discuter</a></li>
-          <li><a href="../memory/pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
-          <li><a href="../memory/pages/connexion.php" class="btn white indigo-text">Login</a></li>
-        </ul>
-      </div>
+        <div class="container">
+            <a href="#" class="brand-logo">Memory</a>
+            <a href="#" class="sidenav-trigger" data-target="mobile-links">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="pages/memory.php">Faire une partie</a></li>
+                <li><a href="pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
+                <li><a href="pages/connexion.php" class="btn white indigo-text">Connexion</a></li>
+            </ul>
+        </div>
     </nav>
-  </div>
+</div>
 
-  <ul class="sidenav" id="mobile-links">
+<ul class="sidenav" id="mobile-links">
     <li><a href="#">Home
-    <i class="material-icons">home </i>
-    </a></li>
+            <i class="material-icons">brightness_3</i>
+        </a></li>
     <li><a href="#">About
-    <i class="material-icons">brightness_6 </i>
-    </a></li>
+            <i class="material-icons">brightness_2</i>
+        </a></li>
     <li><a href="#">Contact
-    <i class="material-icons">mail_outline </i>
-    </a></li>
-    <li><a href="../memory/pages/connexion.php">Discuter
-    <i class="material-icons">insert_comment </i>
-    </a></li>
-    <li><a href="../memory/pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
-    <li><a href="../memory/pages/connexion.php" class="btn white indigo-text">Login</a></li>
-  </ul>
-  
+            <i class="material-icons">brightness_1</i>
+        </a></li>
+    <li><a href="pages/memory.php">Faire une partie
+            <i class="material-icons">brightness_5</i>
+        </a></li>
+    <li><a href="pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
+    <li><a href="pages/connexion.php" class="btn white indigo-text">Connexion</a></li>
+</ul>
 
 
+<main>
 
-  <footer class="page-footer black center">
-      <div class="container">
-          <div class="col l12 s6">
-            <h5 class="white-text">Links</h5>
-              <ul>
-                <li><a href="facebook.com">
-                  <img src="../discussion/images/facebook.png" alt="facebook"/>
-                </a></li>
-                <li><a href="instagram.com">
-                <img src="../discussion/images/instagram.png" alt="instagram"/>
-                </a></li>
-                <li><a href="twitter.com">
-                <img src="../discussion/images/twitter.png" alt="twitter"/>
-                </a></li>
-              </ul>
-          </div>
-      </div>
-          
-    <div class="footer-copyright center">
-      <div class="container">
-        © 2020 Witch Copyright
-      </div>
+</main>
+
+<footer class="page-footer black center">
+    <div class="container">
+        <div class="row valign-wrapper">
+            <div class="col l12 s6">
+                <h5 class="purple-text text-lighten-1">Liens développeuses</h5>
+            </div>
+            <div class="col l12 s6">
+                <ul class="valign-wrapper">
+                    <li><a href="#" class="valign-wrapper">
+                            <span>Alicia</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                    <li><a href="#" class="valign-wrapper">
+                            <span>May</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
     </div>
 
-    </footer>
-            
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-  <script>
-    $(document).ready(function(){
-      $('.sidenav').sidenav();
+    <div class="footer-copyright center">
+        <div class="container">
+            © 2021 Sororité Copyright
+        </div>
+    </div>
+</footer>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
     });
-  </script>
+</script>
 </body>
 </html>
