2459539f60c0674b97b17a72e9a2ae3729b95d87
Cordial <alicia.cordial@laplateforme.io>
2021-01-06 12:05:51 +0100

    board

--- classes/board.php
+++ classes/board.php
@@ -7,7 +7,7 @@ class board {
 
     //function téléchargement cartes
 function downloadCards($pdo){
-    
+
 }
 
     //function display jeu
@@ -31,9 +31,26 @@ function downloadCards($pdo){
    
 
 
-    //function shuffle"
-
-
+    //function shuffle
+    function my_str_shuffle($str)
+    {
+        if ($str == '') {
+            return $str;
+        }
+    
+        $n_left = strlen($str);
+    
+        while (--$n_left) {
+            $rnd_idx = mt_rand(0, $n_left);
+            if ($rnd_idx != $n_left) {
+                $tmp = $str[$n_left];
+                $str[$n_left] = $str[$rnd_idx];
+                $str[$rnd_idx] = $tmp;
+            }
+        }
+    
+        return $str;
+    }
 
 
 
