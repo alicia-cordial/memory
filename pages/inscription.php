695a628f5ab495903bfddf39093d4b02d4a9e43f
Cordial <alicia.cordial@laplateforme.io>
2021-01-06 18:30:57 +0100

    html

--- pages/inscription.php
+++ pages/inscription.php
@@ -7,20 +7,63 @@ $user = new user;
 
 if (isset($_POST['forminscription'])){
 
-  $login = htmlspecialchars($_POST['login']);
-  
-  $password = sha1($_POST['password']);
-  
-  
-  $password2 = sha1($_POST['password2']);
-  
- 
+   
     $user->register($login, $password, $password2);
     $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
  
 }
 ?>
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Entree - Memory</title>
+</head>
+
+<body>
+<div class="navbar-fixed">
+    <nav class="nav-wrapper black">
+        <div class="container">
+            <a href="#" class="brand-logo">Memory</a>
+            <a href="#" class="sidenav-trigger" data-target="mobile-links">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="memory.php">Faire une partie</a></li>
+                <li><a href="inscription.php" class="btn white indigo-text">Inscription</a></li>
+                <li><a href="connexion.php" class="btn white indigo-text">Connexion</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<ul class="sidenav" id="mobile-links">
+    <li><a href="#">Home
+            <i class="material-icons">brightness_3</i>
+        </a></li>
+    <li><a href="#">About
+            <i class="material-icons">brightness_2</i>
+        </a></li>
+    <li><a href="#">Contact
+            <i class="material-icons">brightness_1</i>
+        </a></li>
+    <li><a href="memory.php">Faire une partie
+            <i class="material-icons">brightness_5</i>
+        </a></li>
+    <li><a href="inscription.php" class="btn white indigo-text">Inscription</a></li>
+    <li><a href="connexion.php" class="btn white indigo-text">Connexion</a></li>
+</ul>
 
 
 <main class="valign-wrapper">
@@ -68,3 +111,44 @@ if (isset($erreur))
 
 
 </main>
+
+
+<footer class="page-footer black center">
+    <div class="container">
+        <div class="row valign-wrapper">
+            <div class="col l12 s6">
+                <h5 class="purple-text text-lighten-1">Liens développeuses</h5>
+            </div>
+            <div class="col l12 s6">
+                <ul class="valign-wrapper">
+                    <li><a href="#" class="valign-wrapper">
+                            <span>Alicia</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                    <li><a href="#" class="valign-wrapper">
+                            <span>May</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="footer-copyright center">
+        <div class="container">
+            © 2021 Sororité Copyright
+        </div>
+    </div>
+</footer>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+</script>
+</body>
+</html>
--- pages/profil.php
+++ pages/profil.php
@@ -1,5 +1,78 @@
-<?php?>
+<?php
 
+require_once '../classes/user.php';
+
+session_start();
+
+
+$user = new user;
+
+
+if(isset($_SESSION['user'])){
+
+  $user = $_SESSION['user'];
+
+
+
+if(isset($_POST['formprofil'])){
+  $user->update($_POST['login'], $_POST['password'], $_POST['password2'], $user['id']);
+}
+
+}
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Entree - Memory</title>
+</head>
+
+<body>
+<div class="navbar-fixed">
+    <nav class="nav-wrapper black">
+        <div class="container">
+            <a href="#" class="brand-logo">Memory</a>
+            <a href="#" class="sidenav-trigger" data-target="mobile-links">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="pages/memory.php">Faire une partie</a></li>
+                <li><a href="pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
+                <li><a href="pages/connexion.php" class="btn white indigo-text">Connexion</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<ul class="sidenav" id="mobile-links">
+    <li><a href="#">Home
+            <i class="material-icons">brightness_3</i>
+        </a></li>
+    <li><a href="#">About
+            <i class="material-icons">brightness_2</i>
+        </a></li>
+    <li><a href="#">Contact
+            <i class="material-icons">brightness_1</i>
+        </a></li>
+    <li><a href="pages/memory.php">Faire une partie
+            <i class="material-icons">brightness_5</i>
+        </a></li>
+    <li><a href="pages/inscription.php" class="btn white indigo-text">Inscription</a></li>
+    <li><a href="pages/connexion.php" class="btn white indigo-text">Connexion</a></li>
+</ul>
 
 <main class="valign-wrapper"> 
     
@@ -44,3 +117,43 @@ if (isset($erreur))
 
 
 </main>
+
+<footer class="page-footer black center">
+    <div class="container">
+        <div class="row valign-wrapper">
+            <div class="col l12 s6">
+                <h5 class="purple-text text-lighten-1">Liens développeuses</h5>
+            </div>
+            <div class="col l12 s6">
+                <ul class="valign-wrapper">
+                    <li><a href="#" class="valign-wrapper">
+                            <span>Alicia</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                    <li><a href="#" class="valign-wrapper">
+                            <span>May</span>
+                            <img src="src/twitter.png" alt="twitter">
+                        </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="footer-copyright center">
+        <div class="container">
+            © 2021 Sororité Copyright
+        </div>
+    </div>
+</footer>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+</script>
+</body>
+</html>
