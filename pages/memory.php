2412ccd038106423b96f70088c2779263e8cc9dc
huongmay-nguyenphuoc <huongmay-nguyenphuoc@laplateforme.io>
2021-01-07 12:44:38 +0100

    début mémory

--- pages/memory.php
+++ pages/memory.php
@@ -1,16 +1,18 @@
 <?php
 require_once('../classes/database.php');
-$_GET['nbcards'] = 10;
+$_GET['nbcards'] = 4;
 $nbcards = $_GET['nbcards'];
 $pdo = new database();
 
-$pairs1 = $pdo->Select("SELECT * FROM cards  ORDER BY RAND() LIMIT $nbcards");
-$pairs2 = $pairs1;
-$deck = array_merge($pairs1, $pairs2);
-shuffle($deck);
+    $pairs1 = $pdo->Select("SELECT * FROM cards  LIMIT $nbcards");
+    $pairs2 = $pairs1;
+    $deck = array_merge($pairs1, $pairs2);
+    // shuffle($deck);
 
-if (isset($_POST['submit'])) {
-    echo "yo";
+if (isset($_POST['carte'])) {
+
+$deck[$_POST['carte']]['status'] = 'opened';
+
 }
 ?>
 
@@ -27,34 +29,42 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-<div class="container">
-    <?php
-    echo "<table>";
-    $i = 1;
-    echo "<tr>";
+<main>
+    <div class="container">
+        <?php
+        echo "<table>";
+        $i = 1; //compteur cellules pour ligne
 
-    foreach ($deck as $card) {
-        echo "<td>";
-        if ($card['status'] == 'closed') {
-            echo "<form method='post'><button type='submit' name='submit'><img class='imgcard responsive-image' src='../src/back.png'></button></form>";
-        } elseif ($card['status'] == 'opened') {
-            echo "<img class='imgcard' src=" . $card['img_url'] . ">";
-        } elseif ($card['status'] == 'found') {
-            echo "<img class='imgcard' class='imgtrouvee' src=" . $card['img_url'] . ">";
+        echo "<tr>";
+        for($j= 0; $j < count($deck); $j++) {
+            echo "<td>";
+            if(!isset($deck[$j]['status'])) {
+                $deck[$j]['status'] = 'closed';
+                }
+            if ($deck[$j]['status'] == 'closed') {
+                echo "<form method='post'><button type='input' name='carte' value=".$j."><img class='imgcard responsive-image' src='../src/back.png'></button></form>";
+            } elseif ($deck[$j]['status'] == 'opened') {
+                echo "<img class='imgcard' src=" . $deck[$j]['img_url'] . ">";
+            } elseif ($deck[$j]['status'] == 'found') {
+                echo "<img class='imgcard' class='imgtrouvee' src=" . $deck[$j]['img_url'] . ">";
+            }
+            echo "</td>";
+            $i++;
+
+            if ($i > 5) {
+                echo "</tr><tr>";
+                $i = 1;
+            }
         }
-        echo "</td>";
-        $i++;
 
-        if ($i > 5) {
-            echo "</tr><tr>";
-            $i = 1;
-        }
-    }
 
-    echo "</tr></table>";
-    ?>
-</div>
+        echo "</tr></table>";
+        ?>
+    </div>
+</main>
+
 
 </body>
 
 </html>
+
