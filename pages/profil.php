96bdec1d72a58cb8a7dcad0071184523d88c861c
Cordial <alicia.cordial@laplateforme.io>
2021-01-12 15:47:47 +0100

    profil progression

--- pages/profil.php
+++ pages/profil.php
@@ -57,113 +57,79 @@ if(isset($_SESSION['user'])){
 <main class="valign-wrapper"> 
 
 
-<!--DIFFERENTS NIVEAUX-->
 
-<div class="div_tableau1">
+
+
 <?php
+$score = new score();
+
 $pairsmin = 3; // min niveau
 $pairsmax = 12; // max niveau
-/*
-if (isset($_POST['submit'])) {
-   $_SESSION['level'] = $_POST['submit']; //sauvegarde le choix du niveau dans la session
-    
-}
-*/
-if(isset($_POST['valider'])){
-  $nb_paires = intval($_POST['nb_paires']);
-  $_SESSION['nb_paires'] = $nb_paires ;
-  $_SESSION['level'] = $_POST['nb_paires'] . " " .'paires';
-  //header('Location:memory.php?start');
-}
-?>
 
+if (isset($_POST['submit'])) { //niveau sélectionné
+    $level = $_POST['submit'];
+    $moves = $score->scoreByMoves($level); //récup scores persos par coups
+    $times = $score->scoreByTime($level);//récup scores persos par temps
+    $twoTables = ['coups' => $moves, 'temps' => $times];
+}
+
+?>
 
 <!--TABLEAUX TOP PERSO-->
 
-<form action="" method="post">
-  <select name="nb_paires">
+    <article class="container">
 
-  <?php for($i=3; $i<=12; ++$i):?>
-      <option value=<?= $i ?> ><?= $i ?> paires</option>
-  <?php endfor ?>
+<h1 class="white-text center">Progression Personelle</h1>
 
-  </select>
+<?php if (isset($level)) : ?> <!--Affichage scores du niveau sélectionné-->
+    <h3 class='white-text center'>Niveau : <?php echo $level; ?> </h3>
 
-  <button type="submit" name="valider">Valider</button>
+    <?php
+    foreach ($twoTables as $name => $oneTable) { /*Génération tableaux*/
 
-  </form>
-<?php
+        echo "<table class='centered' id='tableFame'>";
+        echo "<h5>Progression par " . $name . "</h5>";
+        $i = 1;
 
-$bdd = new PDO('mysql:host=localhost;dbname=memory', 'root', '');
+        foreach ($oneTable as $row) {
+            if ($i == 1) {
+                echo "<thead>";
+                foreach ($row as $key => $element) {
+                    echo "<th>" . $key . "</th>";
+                }
+                echo "</thead>";
+                $i = 0;
+            }
+            echo "<tbody>";
+            echo "<tr>";
+            foreach ($row as $cell) {
+                echo "<td>" . $cell . "</td>";
+            }
+            echo "</tr>";
+        }
 
-
-
-$requser = $bdd->prepare("SELECT login, score.id as 'n°', niveau, time as 'chrono', nb_coup as 'coups' FROM score inner join utilisateurs on score.id_utilisateur = utilisateurs.id WHERE niveau ORDER BY score.nb_coup ASC LIMIT 10");
-$requser->execute();
-
-$i=0;
-
-echo "<table>" ;
-
-while ($result = $requser->fetch(PDO::FETCH_ASSOC))
-{
-    if ($i == 0)
-  {
-
-    foreach ($result as $key => $value)
-    {
-      echo "<th>$key</th>";
+        echo "</tbody>";
+        echo "</table>";
     }
-    $i++;
-
-  }
-
-  echo "<tr>";
-  foreach ($result as $key => $value) {
-    if ($key == "login"){
-      date_default_timezone_set('Europe/Paris');
-      $value =  date("d-m-Y", strtotime($value));  ;
-    echo "<td>$value</td>";
-    }
-    else
-      echo "<td>" .nl2br($value). "</td>";
-  }
-  echo "</tr>";
-}
-
-echo "</table>";
-
- ?>
-
-<?php
-if (isset($erreur))
-{
-  echo $erreur;
-}
-?>
-
-</div>
-
-            
-    
-<div class="row">
-
-<?php if (!empty($errors)): ?>
-    <div>
-        <?php foreach ($errors as $error) {
-            echo '<p class="red-text">' . $error . '</p>';
-        } ?>
-    </div>
-<?php elseif (isset($success)): ?>
-    <div>
-        <?php {
-            echo '<p class="green-text">' . $success . '</p>';
-        } ?>
-    </div>
-  <?php endif; ?>
+    ?>
 
 
 
+
+<!--DIFFERENTS NIVEAUX-->
+
+<?php else : ?> <!--Affichage boutons sélection niveau-->
+    <h5 class="white-text"><em>Veuillez sélectionner un nombre de paires</em></h5>
+    <form action="profil.php" method="post" class="row">
+        <?php for ($i = $pairsmin; $i <= $pairsmax; $i++) : ?>
+                <div class='col'>
+                <button class='black white-text btn-large buttonlevel' type='submit' name='submit' value='<?php echo $i ?>'> <?php echo $i ?> </button>
+                </div>
+        <?php endfor; ?>
+    </form>
+<?php endif; ?>
+
+</article>
 
 
 <!--FORMULAIRE UPDATE-->
