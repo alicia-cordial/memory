4c094f459837122dafe38df63b4a79e11977a741
Cordial <alicia.cordial@laplateforme.io>
2021-01-15 10:39:04 +0100

    update

--- memory.sql
+++ memory.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 14 jan. 2021 à 18:28
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Host: localhost:3306
+-- Generation Time: Jan 15, 2021 at 09:38 AM
+-- Server version: 5.7.30
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -18,24 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `memory`
+-- Database: `memory`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cards`
+-- Table structure for table `cards`
 --
 
-DROP TABLE IF EXISTS `cards`;
-CREATE TABLE IF NOT EXISTS `cards` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `img_url` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL,
+  `img_url` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `cards`
+-- Dumping data for table `cards`
 --
 
 INSERT INTO `cards` (`id`, `img_url`) VALUES
@@ -55,21 +52,19 @@ INSERT INTO `cards` (`id`, `img_url`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `score`
+-- Table structure for table `score`
 --
 
-DROP TABLE IF EXISTS `score`;
-CREATE TABLE IF NOT EXISTS `score` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `score` (
+  `id` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `niveau` int(11) NOT NULL,
   `nb_coup` int(11) NOT NULL,
-  `time` time NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `score`
+-- Dumping data for table `score`
 --
 
 INSERT INTO `score` (`id`, `id_utilisateur`, `niveau`, `nb_coup`, `time`) VALUES
@@ -101,32 +96,76 @@ INSERT INTO `score` (`id`, `id_utilisateur`, `niveau`, `nb_coup`, `time`) VALUES
 (27, 19, 7, 26, '00:00:31'),
 (28, 19, 12, 58, '00:01:09'),
 (29, 19, 6, 20, '00:00:31'),
-(30, 19, 8, 38, '00:01:05');
+(30, 19, 8, 38, '00:01:05'),
+(31, 20, 5, 20, '00:00:20'),
+(32, 20, 12, 70, '00:01:30');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateurs`
+-- Table structure for table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `utilisateurs`
+-- Dumping data for table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
 (16, 'may', '$2y$10$ZwDxs20idGnYJu3bYnVike29l5jmb7dmKl0TfXQ0PuXJbFlmkMHwG'),
 (17, 'bat', '$2y$10$AAC9LUyYd4ka.84jEWj9.OgS4dJV9aL/16mcaJ3e3cR6dwvo44S/.'),
 (18, 'leo1', '$2y$10$Emar5Rs8Pgl.lA0fXbFu6O00gxSsf4EFQG6ve49GG8O5DXj8cBmca'),
-(19, 'leratfuryo', '$2y$10$KB5wsrxX.U1Lnci9Y/D9N.fX/slPUcwOZNC0bKQcjSYVf1WNVb6Iu');
-COMMIT;
+(19, 'leratfuryo', '$2y$10$KB5wsrxX.U1Lnci9Y/D9N.fX/slPUcwOZNC0bKQcjSYVf1WNVb6Iu'),
+(20, 'Alicia1802', '$2y$10$O6s3dYRKnYFJHWbpUG/gPe6pLnQ6wDth1.K85bljnJba.IrQijfxW');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
